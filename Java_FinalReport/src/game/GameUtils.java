de1b16be9be84f560059d6effbf1ad3958b34f36
Chihiro043 <yui.23611@gmail.com>
2025-06-15 17:34:02 +0800

    Update GameUtils.java

--- Java_FinalReport/src/game/GameUtils.java
+++ Java_FinalReport/src/game/GameUtils.java
@@ -9,6 +9,18 @@ import java.util.List;
 
 public class GameUtils {
 
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+
+    public static void printColor(String text, String color) {
+        System.out.println(color + text + RESET);
+    }
+
     public static void clearScreen() {
         for (int i = 0; i < 30; i++) {
             System.out.println();
